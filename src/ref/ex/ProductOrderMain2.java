959c01e47ac777385a81a858932f421275a3e129
Yongwook Choi <dyddnr25@gmail.com>
2024-04-09 03:33:28 +0900

    ref

--- src/ref/ex/ProductOrderMain2.java
+++ src/ref/ex/ProductOrderMain2.java
@@ -11,9 +11,9 @@ public class ProductOrderMain2 {
 
         /*//풀이
         ProductOrder[] orders = new ProductOrder[3];
-        orders[1] = createOrder("두부", 2000, 2);
-        orders[2] = createOrder("김치", 5000, 1);
-        orders[3] = createOrder("콜라", 1500, 2);
+        orders[0] = createOrder("두부", 2000, 2);
+        orders[1] = createOrder("김치", 5000, 1);
+        orders[2] = createOrder("콜라", 1500, 2);
 
          */
 
